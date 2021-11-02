138cf6e6aadd173fbba59680a44ddf8f00d4f417
Lea Blatnik <lea.blatnik7@gmail.com>
2021-11-02 11:34:43 +0100

    initial commit, there's a lot of them

---
+++ main.py
@@ -0,0 +1,30 @@
+import random
+
+secret = random.randint(1, 30)
+guess = None
+attempts = 0
+secret_number_file = "secret_number_best_score.txt"
+
+with open(secret_number_file, "r") as attempts_file:
+    attempts_from_file = int(attempts_file.read())
+    print(f"Best score: {attempts_from_file} attempts.")
+
+while True:
+    guess = int(input("What is your guess? "))
+    attempts = attempts + 1
+
+    if guess == secret:
+        print(f"Congratulations, it is {secret}")
+        print(f"Number of attempts: {attempts}")
+        if attempts < attempts_from_file:
+            with open(secret_number_file, "w") as attempts_file:
+                attempts_file.write(str(attempts))
+        break
+
+    hint = None
+    if guess > secret:
+        hint = "smaller"
+    else:
+        hint = "bigger"
+
+    print(f"Sorry, your guess is incorrect, try something {hint}")
