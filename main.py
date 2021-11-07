fd589f70d328c70266cf2c77802f03f60f77e859
Lea Blatnik <lea.blatnik7@gmail.com>
2021-11-07 18:42:22 +0100

    Add log of all attempts

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import json
 import random
 
 secret = random.randint(1, 30)
@@ -9,6 +10,10 @@ with open(secret_number_file, "r") as attempts_file:
     attempts_from_file = int(attempts_file.read())
     print(f"Best score: {attempts_from_file} attempts.")
 
+with open ("secret_number_attempts.json", "r") as all_attempts_file:
+    logged_attempts = json.loads(all_attempts_file.read())
+    print(f"The number of all game attempts: {logged_attempts}")
+
 while True:
     guess = int(input("What is your guess? "))
     attempts = attempts + 1
@@ -16,6 +21,9 @@ while True:
     if guess == secret:
         print(f"Congratulations, it is {secret}")
         print(f"Number of attempts: {attempts}")
+        with open ("secret_number_attempts.json", "w") as score_file:
+            score_file.write(json.dumps(logged_attempts.append(attempts)))
+
         if attempts < attempts_from_file:
             with open(secret_number_file, "w") as attempts_file:
                 attempts_file.write(str(attempts))
